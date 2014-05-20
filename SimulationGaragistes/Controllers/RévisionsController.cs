82920d9769cd60c47a81036636179a9b36119bf9
Scoms <wieser.laurent@gmail.com>
2014-05-20 21:06:06 +0200

    Ajout des pannes aléatoires

--- SimulationGaragistes/Controllers/RévisionsController.cs
+++ SimulationGaragistes/Controllers/RévisionsController.cs
@@ -77,12 +77,91 @@ namespace SimulationGaragistes.Controllers
         public ActionResult Delete(int id)
         {
             Révisions revision = this._serviceR.findById(id);
-            id = (int)revision.modele_id;
+            bool aleatoire = revision.modele_id == null;
+            if(!aleatoire)
+             id = (int)revision.modele_id;
             if (revision != null)
             {
                 this._serviceR.Delete(revision);
             }
+            if(aleatoire)
+                return RedirectToAction("IndexAleatoire");
             return RedirectToAction("seeRevisions", "Modeles", new { id = id, errors = this._eh.getErrors() });
+        }
+
+        /*
+         * Partie effectué tardivement d'ou le coté "sale" du code
+         * */
+        [HttpGet]
+        public ActionResult IndexAleatoire()
+        {
+            List<Révisions> lRev = this._serviceR.findAll().Where(r => r.modele_id == null).ToList();
+            return View(lRev);
+        }
+
+        [HttpGet]
+        public ActionResult InsertAleatoire(int id = 0)
+        {
+            Révisions revision = new Révisions();
+            if (id != 0)
+            {
+                revision = this._serviceR.findById(id);
+                if (revision == null)
+                {
+                    revision.km = 0;
+                    revision.modele_id = null;
+                    return RedirectToAction("IndexAleatoire");
+                }
+            }
+            return View(revision);
+        }
+
+        [HttpPost]
+        public ActionResult InsertAleatoire(Révisions revision)
+        {
+            string message = String.Empty;
+            revision.km = 0;
+            revision.modele_id = null;
+            int maxCount = 1;
+            //int id = (int)this._serviceR.findById(revision.id).modele_id;
+            using (SimulationGaragistesEntities context = new SimulationGaragistesEntities())
+            {
+                if (revision.id > 0)
+                {
+                    maxCount = 2;
+                }
+                if(revision.defaultTime <= 0)
+                {
+                    this._eh.addError("Durée invalide");
+                }
+                if (context.Révisions.Where(r => r.label.ToUpper().Equals(revision.label)).Count() == maxCount)
+                {
+                    this._eh.addError("La panne existe déjà");
+                }
+
+                if (!this._eh.hasErrors())
+                {
+                    if (revision.id != 0)
+                    {
+                        TempData["success"] = "La panne à bien été modifiée";
+                        context.Entry(revision).State = System.Data.Entity.EntityState.Modified;
+                    }
+                    else
+                    {
+                        TempData["success"] = "La panne à bien été ajoutée";
+                        context.Révisions.Add(revision);
+                    }
+                    context.SaveChanges();
+                    return RedirectToAction("IndexAleatoire");
+                    return View();
+                }
+                else
+                {
+                    TempData["errors"] = this._eh.getErrors();
+                    return View(revision);
+                }
+            }
+            return View(revision);
         }
 	}
 }
